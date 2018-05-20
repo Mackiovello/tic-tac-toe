32f40092028cbe4e487c980f7414c8c961269aad
Mackiovello <elandroe@icloud.com>
2018-05-20 10:41:39 +0200

    Start sketching on getting input from user

--- tic_tac_toe/src/main.rs
+++ tic_tac_toe/src/main.rs
@@ -1,5 +1,16 @@
+use std::io::{self, BufRead};
+
 fn main() {
+    let line = read_line();
+    println!("{}", line);
     print_board();
+}
+
+fn get_coordinate_from_user() -> Coordinate {
+    // println!("Enter a coordinate in the format x, y:");
+    // let input = read_line();
+    // let vec = input.split_whitespace().collect();
+    // Coordinate { x: vec[0], y: vec[1] }
 }
 
 fn print_board() {
@@ -14,4 +25,16 @@ fn print_board() {
                 |     |     
           2  -  |  -  |  -  
                 |     |     ");
+}
+
+fn read_line() -> String {
+    let mut line = String::new();
+    let stdin = io::stdin();
+    stdin.lock().read_line(&mut line).unwrap();
+    line
+}
+
+struct Coordinate {
+    x: i32,
+    y: i32
 }
