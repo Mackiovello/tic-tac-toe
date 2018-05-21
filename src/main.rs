2b5700a37844e9ff9fc48b03c1b0a97df4eb27c6
Mackiovello <elandroe@icloud.com>
2018-05-21 06:42:19 +0200

    Add TODO and needed structs and enums

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,10 @@ fn main() {
 
 fn get_coordinate_from_user() -> Coordinate {
     println!("Enter a coordinate in the format x,y:");
+
+    // TODO: Add input validation and split validation
+    //       into a separate function
+   
     let input = read_line();
     let vec = input.split(',').collect::<Vec<&str>>();
     let x = vec[0].trim().parse::<u8>().unwrap();
@@ -34,6 +38,16 @@ fn read_line() -> String {
     let stdin = io::stdin();
     stdin.lock().read_line(&mut line).unwrap();
     line
+}
+
+struct Board {
+    grid: Vec<Vec<u8>>
+}
+
+enum CoordinateValue {
+    UserOne,
+    UserTwo,
+    Empty
 }
 
 struct Coordinate {
